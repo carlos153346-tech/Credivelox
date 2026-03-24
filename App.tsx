a3f3e3e8ece0a64db45848d804b41708e0a621e1
carlos153346-tech <carlos153346@gmail.com>
2026-03-24 15:25:22 -0300

    feat: Track 'InitiateCheckout' event for Pix checkout
    Adds Facebook Pixel tracking for the 'InitiateCheckout' event when a user proceeds to the Pix checkout. This helps measure conversion rates and optimize marketing efforts.

--- App.tsx
+++ App.tsx
@@ -22,6 +22,9 @@ const App: React.FC = () => {
   };
 
   const handleFinalize = () => {
+    if (typeof (window as any).fbq === 'function') {
+      (window as any).fbq('track', 'InitiateCheckout');
+    }
     window.location.href = 'https://pay.checkoutpixes.com/checkout/cmmqp606109y61ymygpuev28o?offer=Z5NELE5';
   };
 
